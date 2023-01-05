4fa5b275979e9b9001bc30be0f2e0edb382b75c0
Martin-Borchert <108767551+ConnerMart@users.noreply.github.com>
2023-01-04 21:01:08 -0700

    added buttons for weapons with predeterimined numbers of dice

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # _Star Trek Adventures_ Roll Resolver
 
-An app to roll dice and display their results in game terms for the _Star Trek Adventures_ Tabletop RPG.
+An app to roll dice and display their results in game terms for combat encounters in the _Star Trek Adventures_ Tabletop RPG.
--- script.js
+++ script.js
@@ -6,18 +6,18 @@ let damageResult = 0;
 let effectCount = 0;
 
 const rollButton = document.querySelector("#roll-button");
-rollButton.addEventListener("click", function rollAllDice() {
+rollButton.addEventListener("click", function rollNumberOfDice() {
   var numberOfDice = document.getElementById("numberOfDice").value;
-  console.log(numberOfDice);
   allResults = [];
   for (let i = 1; i <= numberOfDice; i++) {
     rand = Math.floor(Math.random() * d6.length);
     d6result = d6[rand];
     allResults.push(d6result);
   }
-  console.log(allResults);
   allResults.forEach(interpretResults);
   console.log(`
+    rolling ${numberOfDice}d6...
+    ${allResults.join(", ")}
     Damage Rolled: ${damageResult}
     Effects Rolled: ${effectCount}
     `);
@@ -47,6 +47,41 @@ function interpretResults(rollResult) {
       damageResult += 1;
       effectCount += 1;
       break;
-    // TODO: add default case(?)/message if input is left blank
+    // TODO: add default case(?)/message if input is left blank (numberofDice?)
   }
 }
+
+function rollWeapon(damageDice, name) {
+  var numberOfDice = damageDice;
+  allResults = [];
+  for (let i = 1; i <= numberOfDice; i++) {
+    rand = Math.floor(Math.random() * d6.length);
+    d6result = d6[rand];
+    allResults.push(d6result);
+  }
+  allResults.forEach(interpretResults);
+  console.log(`
+    ${name}: rolling ${numberOfDice}d6...
+    ${allResults.join(", ")}
+    Damage Rolled: ${damageResult}
+    Effects Rolled: ${effectCount}
+    `);
+  damageResult = 0;
+  effectCount = 0;
+}
+
+// TODO: add effects
+// Unarmed Strike = 1d6, Knockdown
+// Knife/Dagger = 1d6, Vicious 1
+// Blade (Sword, Mek'leth, etc.) = 2d6, Vicious 1
+// Heavy Blade (Bat'leth, Kar'takin, Lirpa) = 3d6, Vicious 1
+// Bludgeon = 2d6, Knockdown
+// Phaser Type-1 = 2d6
+// Phaser Type-2 = 3d6
+// Phaser Type-3 (Phaser Rifle) = 4d6
+// Pulse Grenade = 4d6, Area
+// Andorian Plasma Rifle = 4d6, Intense
+// Phase Pistol = 3d6
+// Particle Rifle = 4d6
+// Disruptor Pistol = 3d6, Vicious 1
+// Disruptor Rifle = 4d6, Vicious 1
